015c342a6ecc4f9c420250450a32040231e969a9
AVVS <v@aminev.me>
2016-02-04 00:08:56 +0300

    feat: add timeout to AMQPTransport.connect

--- src/transport.js
+++ src/transport.js
@@ -1,10 +1,8 @@
-const Promise = require('bluebird');
 const AMQPTransport = require('ms-amqp-transport');
 
 module.exports = function disposer(host, port) {
   return AMQPTransport
     .connect({ connection: { host, port } })
-    .disposer(amqp => {
-      return amqp.close();
-    });
+    .timeout(10000)
+    .disposer(amqp => amqp.close());
 };
